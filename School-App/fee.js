59dadd641deea7797ef5f2c2af8b37548c860fe3
prudhvisurya996 <prudhvisurya996@gmail.com>
2024-01-20 12:05:21 +0530

    1205200124

---
+++ School-App/fee.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    // Fetch values from the form
+    const selectedClass = document.getElementById('classSelect').value;
+    const selectedSection = document.getElementById('sectionSelect').value;
+    const rollNumber = document.getElementById('rollNumber').value;
+  
+    // You can use these values for further processing or validation
+    console.log('Selected Class:', selectedClass);
+    console.log('Selected Section:', selectedSection);
+    console.log('Roll Number:', rollNumber);
+    // Add your logic here for handling the form data
+  }
+  
